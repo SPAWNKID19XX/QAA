f4ca5dba4de088031d910f6a009fedc1cb9c892c
spawnkid1987 <borisisac@gmail.com>
2026-04-20 16:28:31 +0100

    [ADD] bug report

--- backend/apps/accounts/tests/api/test_signup.py
+++ backend/apps/accounts/tests/api/test_signup.py
@@ -4,6 +4,15 @@ import pytest
 @pytest.mark.django_db
 class TestSignUpAPI:
   def test_signup_with_valid_data(self, api_client, user_payload_factory):
-    response = api_client.post("/api/auth/register/", data=user_payload_factory(), format="json")
+    response = api_client.post("/api/auth/register/", data=user_payload_factory(), format="json")    
+    assert response.status_code == 201
+    assert "password" not in response.json()['user']
 
-    assert response.status_code in [200,201]
+    user_id = response.json()['user']['id']
+    
+    user = get_user_model().objects.filter(id=user_id).first()
+    assert user is not None
+    assert user.id == user_id
+    assert user.email == response.json()['user']['email']
+    assert user.username == response.json()['user']['username']
+    
